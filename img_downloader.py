68ac82b62f89791f24ea29fd49e12e71133fc7cd
Henrique Andrade <henriquearaujoandrade@gmail.com>
2018-09-13 14:58:32 +0200

    Support to images that src is not the full path

--- img_downloader.py
+++ img_downloader.py
@@ -1,23 +1,41 @@
 import os
 import requests
 import urllib
+from urllib.parse import urlparse 
 from bs4 import BeautifulSoup
 
-url = input('URL: ')
-html = requests.get(url)
-html_text = html.text
-soup = BeautifulSoup(html_text, 'html.parser')
+def get_extension(img_url):
+    path = urlparse(img_url).path
+    ext = os.path.splitext(path)[1]
+    return ext
 
-imgs = soup.findAll('img', {'src': True})
+def main():
+    url = input('URL: ')
+    html = requests.get(url)
+    html_text = html.text
+    soup = BeautifulSoup(html_text, 'html.parser')
 
-if not os.path.isdir('img'):
-    os.mkdir('img')
-i = 0
-name = input('Name to save image: ')
-for img in imgs:
-    img_url = img['src']
-    if img_url.startswith('http'): 
+    imgs = soup.findAll('img', {'src': True})
+
+    if not os.path.isdir('img'):
+        os.mkdir('img')
+
+    i = 0
+    name = input('Name to save image: ')
+    for img in imgs:
+        img_url = img['src']
+        if not img_url.startswith('http'):
+            img_url = url + img_url
+        print('Saving image: ' + img_url) 
         i = i + 1
-        f = open('img/' + name + str(i) + '.jpg','wb')
-        f.write(urllib.request.urlopen(img_url).read())
+        ext = get_extension(img_url)
+        f = open('img/' + name + ' - ' + str(i) + ext, 'wb')
+        try:
+            f.write(urllib.request.urlopen(img_url).read())
+        except urllib.error.HTTPError:
+            print('Image ' + img_url + ' not found')
+
         f.close()
+
+if __name__ == '__main__':
+    main()
