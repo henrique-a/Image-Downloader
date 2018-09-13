6d74f5ddef0fccf98504153db6c3c3c453e05439
Henrique Andrade <henriquearaujoandrade@gmail.com>
2018-09-13 20:48:00 +0200

    Downloading images in links

--- img_downloader.py
+++ img_downloader.py
@@ -4,38 +4,49 @@ import urllib
 from urllib.parse import urlparse 
 from bs4 import BeautifulSoup
 
+i = 0 # Image counter
 def get_extension(img_url):
     path = urlparse(img_url).path
     ext = os.path.splitext(path)[1]
     return ext
+
+def save_image(url, img_url, name):
+    if not img_url.startswith('http'):
+        img_url = url + img_url
+    print('Saving image: ' + img_url) 
+    global i
+    i = i + 1
+    ext = get_extension(img_url)
+    f = open('img/' + name + ' - ' + str(i) + ext, 'wb')
+    try:
+        f.write(urllib.request.urlopen(img_url).read())
+    except urllib.error.HTTPError:
+        print('Image ' + img_url + ' not found')
+    except ConnectionResetError:
+        print('Connection interrupted')
+
+    f.close()    
 
 def main():
     url = input('URL: ')
     html = requests.get(url)
     html_text = html.text
     soup = BeautifulSoup(html_text, 'html.parser')
-
-    imgs = soup.findAll('img', {'src': True})
-
     if not os.path.isdir('img'):
         os.mkdir('img')
 
-    i = 0
+    imgs = soup.findAll('img', {'src': True})
     name = input('Name to save image: ')
     for img in imgs:
         img_url = img['src']
-        if not img_url.startswith('http'):
-            img_url = url + img_url
-        print('Saving image: ' + img_url) 
-        i = i + 1
-        ext = get_extension(img_url)
-        f = open('img/' + name + ' - ' + str(i) + ext, 'wb')
-        try:
-            f.write(urllib.request.urlopen(img_url).read())
-        except urllib.error.HTTPError:
-            print('Image ' + img_url + ' not found')
+        save_image(url, img_url, name)
 
-        f.close()
+    # Find images in links
+    links = soup.findAll('a', {'href': True})
+    for link in links:
+        if link.findAll('img'):
+            img_url = link['href']
+            save_image(url, img_url, name)
 
 if __name__ == '__main__':
     main()
