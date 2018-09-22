212cf36c5d2ff3c9b74802b0f17a8600f2a8b032
Henrique Andrade <henriquearaujoandrade@gmail.com>
2018-09-22 22:14:54 +0200

    Now also downloads images added via Javascript

--- img_downloader.py
+++ img_downloader.py
@@ -1,37 +1,49 @@
 import os
 import requests
 import urllib
-from urllib.parse import urlparse 
 from bs4 import BeautifulSoup
+import dryscrape
 
 i = 0 # Image counter
 def get_extension(img_url):
-    return '.' + img_url.split('.')[-1]
+    formats = ['.jpg', '.jpeg', '.png', '.tiff', '.gif', '.svg']
+    ext = '.' + img_url.split('.')[-1]
+    if not ext in formats:
+        return '' 
+    return ext 
 
 def save_image(url, img_url, name):
-    if not img_url.startswith('http'):
-        img_url = url + img_url
-    print('Saving image: ' + img_url) 
-    global i
-    i = i + 1
-    ext = get_extension(img_url)
-    f = open('img/' + name + ' - ' + str(i) + ext, 'wb')
-    try:
-        f.write(urllib.request.urlopen(img_url).read())
-    except urllib.error.HTTPError:
-        print('Image ' + img_url + ' not found')
-    except ConnectionResetError:
-        print('Connection interrupted')
-    except urllib.error.URLError:
-        print('Operation timed out')
 
-    f.close()    
+    if img_url.startswith('//'):
+        img_url = 'http:' + img_url
+
+    elif img_url.startswith('/'):
+        img_url = url + img_url
+
+    ext = get_extension(img_url)
+
+    if ext:
+        print('Saving image: ' + img_url) 
+        global i
+        i = i + 1
+        f = open('img/' + name + ' - ' + str(i) + ext, 'wb')
+        try:
+            f.write(urllib.request.urlopen(img_url).read())
+        except urllib.error.HTTPError:
+            print('Image ' + img_url + ' not found')
+        except ConnectionResetError:
+            print('Connection interrupted')
+        except urllib.error.URLError:
+            print('Operation timed out')
+
+        f.close()    
 
 def main():
     url = input('URL: ')
-    html = requests.get(url)
-    html_text = html.text
-    soup = BeautifulSoup(html_text, 'html.parser')
+    session = dryscrape.Session()
+    session.visit(url)
+    response = session.body()
+    soup = BeautifulSoup(response, "html.parser")
     if not os.path.isdir('img'):
         os.mkdir('img')
 
@@ -43,13 +55,13 @@ def main():
         if link.findAll('img'):
             img_url = link['href']
             save_image(url, img_url, name)
-
+    
     # Find images in <img> tags
     imgs = soup.findAll('img', {'src': True})
     for img in imgs:
+        print(img)
         img_url = img['src']
         save_image(url, img_url, name)
-
     
 if __name__ == '__main__':
     main()
