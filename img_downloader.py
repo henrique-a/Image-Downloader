7638b0897664d45e7b8245df81210a9197e143cd
Henrique Andrade <henriquearaujoandrade@gmail.com>
2018-09-28 22:11:52 +0200

    Working with threads

--- img_downloader.py
+++ img_downloader.py
@@ -1,16 +1,35 @@
 import os
+from queue import Queue
+import threading as th
 import urllib
 import urllib.request
 from bs4 import BeautifulSoup
 import dryscrape
 
 i = 0 # Image counter
+NUMBER_OF_THREADS = 8
+queue = Queue()
+
 def get_extension(img_url):
     formats = ['.jpg', '.jpeg', '.png', '.tiff', '.gif', '.svg']
     ext = '.' + img_url.split('.')[-1]
     if not ext in formats:
         return '' 
     return ext 
+
+# Create worker threads
+def create_workers(url, name):
+    for _ in range(NUMBER_OF_THREADS):
+        t = th.Thread(target=work, args=(url, name))
+        t.start()
+
+# Do the next job in the queue
+def work(url, name):
+    while not queue.empty():
+        img_url = queue.get()
+        save_image(url, img_url, name)
+        queue.task_done()
+
 
 def save_image(url, img_url, name):
 
@@ -54,13 +73,15 @@ def main():
     for link in links:
         if link.findAll('img'):
             img_url = link['href']
-            save_image(url, img_url, name)
+            queue.put(img_url)
     
     # Find images in <img> tags
     imgs = soup.findAll('img', {'src': True})
     for img in imgs:
         img_url = img['src']
-        save_image(url, img_url, name)
-    
+        queue.put(img_url)
+
+    create_workers(url, name)
+
 if __name__ == '__main__':
     main()
